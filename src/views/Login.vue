db001c289fc2b115a1310426a4f3f08407294154
Mohammad reza <mamalreza83@gmail.com>
2023-08-26 12:55:03 +0400

    add login from

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,63 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 
+    let username = ref('')
+    let password = ref('')
+    let email = ref('')
+    async function login() {
+      const response = await fetch('http://localhost:3000/users/login',{headers : {"Content-Type": "application/json" } ,
+         method :"POST" ,
+         body : JSON.stringify({username : username.value , email : email.value , password:password.value }) })
+        if (response.status ==200) {
+            console.log(response.json());
+            
+        } else {
+            
+        }
+        
+        
+    }
+    
 </script>
 
 <template>
     <div class="loginForm">
         <h1>Login</h1>
-        <input type="text" placeholder="username">
+        <input type="text" placeholder="username" v-model="username">
         <br>
-        <input type="text" placeholder="email">
+        <input type="text" placeholder="email" v-model="email">
         <br>
-        <input type="text" placeholder="password">
-
+        <input type="text" placeholder="password" v-model="password">
+        <br>
+        <button @click="login">Login</button>
     </div>
 
 </template>
 
-<style>
+<style scoped>
+    .loginForm{
+        position: relative;
+        /* left: 30%; */
+        border: 2px solid rgb(171, 88, 238);
+        border-radius: 10px;
+        width: 25%;
+        text-align: center;
+        margin: 10% 35%;
+    }
 
+    .loginForm input{
+        border-top: none;
+        border-left: none;
+        border-right: none;
+        margin: 10px;
+    }
+
+    .loginForm button{
+        border: none;
+        width: 128px;
+        border-radius: 5px;
+        background: linear-gradient(109.6deg, rgb(128, 144, 233) 17.4%, rgb(171, 88, 238) 52.4%, rgb(255, 129, 246) 91%);
+        height: 32px;
+        margin: 10px;
+    }
 </style>

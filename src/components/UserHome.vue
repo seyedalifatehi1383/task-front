96b4b3a8725ac3100c1d5f07bb9c545a4c07bb6d
Mohammad reza <mamalreza83@gmail.com>
2023-08-26 14:19:19 +0400

    add user home

---
+++ src/components/UserHome.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { Autenticate } from "../stores/counter";
+const Autent = Autenticate()
+let restualt :any
+let tasks = ref([])
+
+onMounted(async () => {
+     restualt = await fetch('http://localhost:3000/myTasks' , {headers : {'Authorization' : 'Bearer ' + Autent.token }})
+     
+     
+  });
+</script>
+
+<template>
+<h1>{{ restualt }}</h1>
+
+</template>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 import DefualtHome from '@/components/DefualtHome.vue';
+import UserHome from '@/components/UserHome.vue';
+import { Autenticate } from "../stores/counter";
+
+const Autent = Autenticate()
+
 </script>
 
 <template>
-    <DefualtHome/>
-    
+    <DefualtHome v-if="Autent.token ==''"/>
+    <div v-else>
+      <UserHome/>
+    </div>
 </template>
 
 <style scoped>

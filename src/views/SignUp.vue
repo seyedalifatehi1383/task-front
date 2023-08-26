f1ac94df5a7927b67211247aaf0b4015ce67b346
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-08-26 13:49:48 +0330

    trying to connect signup route to the databse

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,19 +1,43 @@
 
 <template>
     <div class="signUpForm">
-        <h1>Login</h1>
+        <h1>SignUp</h1>
         <input type="text" placeholder="username" v-model="username">
         <br>
         <input type="text" placeholder="email" v-model="email">
         <br>
         <input type="text" placeholder="password" v-model="password">
         <br>
-        <button @click="login">Login</button>
+        <button @click="signup">SignUp</button>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 
+    let username = ref('')
+    let password = ref('')
+    let email = ref('')
+    let responsebody : any
+
+    async function signup() {
+        const response = await fetch('http://localhost:3000/signup', {
+            headers : {"Content-Type": "application/json" } ,
+            method :"POST" ,
+            body : JSON.stringify({
+                username : username.value,
+                email : email.value,
+                password: password.value
+            }) 
+        })
+        
+        if (response.status == 200) {
+            responsebody = await response.json()   
+
+        } else {
+            console.log(response);
+        }
+    }
 </script>
 
 <style scoped>
@@ -41,5 +65,6 @@
         background: linear-gradient(109.6deg, rgb(128, 144, 233) 17.4%, rgb(171, 88, 238) 52.4%, rgb(255, 129, 246) 91%);
         height: 32px;
         margin: 10px;
+        color: white;
     }
 </style>

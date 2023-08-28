3513f585d8afa2dfe463e0d3768e612b774d9ed8
Seyed Ali Fatehi <afatehi07@gmail.com>
2023-08-28 14:52:11 +0330

    adding edit and delete icon fro users task in manage users panel in subadmin dashboard

--- src/views/ManageUsers.vue
+++ src/views/ManageUsers.vue
@@ -9,10 +9,6 @@
 
                 <h1>{{ user.username }}</h1>
 
-                <!-- <div class="status">
-
-                </div> -->
-
                 <div class="addTask" title="add task for user">
                     <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor"
                         class="bi bi-file-earmark-plus-fill" viewBox="0 0 16 16" @click="showAddTaskModal = true">
@@ -36,8 +32,29 @@
 
             <div class="seeTasks" v-if="user.showTasks" style="margin: 15px;">
                 <details v-for="tasks in allTasks[mainIndex]">
-                    <summary> {{ tasks.title }} </summary>
+                    <summary>
+                        <span> {{ tasks.title }} </span>
+
+                        <div>
+                            <svg @click="" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                                class="bi bi-trash-fill" viewBox="0 0 16 16" title="delete task">
+                                <path
+                                    d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+                            </svg>
+
+                        </div>
+
+                        <svg @click="" style="margin-right: 10px;" xmlns="http://www.w3.org/2000/svg" width="25" height="25"
+                            fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16" title="edit task">
+                            <path
+                                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                            <path fill-rule="evenodd"
+                                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                        </svg>
+                    </summary>
+
                     <blockquote> {{ tasks.desc }} </blockquote>
+
                 </details>
             </div>
         </div>
@@ -113,11 +130,19 @@ function closeModal() {
 
 .seeTasks details summary {
     font-size: 25px;
+}
+
+.seeTasks details summary span {
+    /* font-size: 25px; */
     cursor: pointer;
 }
 
 .seeTasks details blockquote {
     font-size: 20px;
+}
+
+.seeTasks details svg {
+    float: right;
 }
 
 .addTask svg {

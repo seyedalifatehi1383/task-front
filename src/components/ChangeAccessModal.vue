d479be0ac583cc4cb3c6aa941aa3a46b61725e5f
Mohammad reza <mamalreza83@gmail.com>
2023-09-03 14:26:26 +0400

    fix and add model for change promote

---
+++ src/components/ChangeAccessModal.vue
@@ -0,0 +1,111 @@
+<template>
+    <div class="modal-bg" @click="closeAccessModal"></div>
+    <div class="modalContainer">
+        <div class="close-button" @click="closeAccessModal">
+            <svg style="color: rgb(171, 88, 238);" xmlns="http://www.w3.org/2000/svg" height="1em"
+                viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                <path style="color: rgb(171, 88, 238);"
+                    d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+            </svg>
+        </div>
+
+        <p style="margin: 20px; font-size: 35px; color: rgb(171, 88, 238);"> Are you sure to change this user's access </p>
+        <button @click="changeAccess" style="margin-bottom: 10px; margin-right: 10px; margin-left: 10px;"> Yes </button>
+        <button @click="closeAccessModal" style="margin-bottom: 10px; margin-left: 10px; margin-right: 10px;"> No </button>
+        <!-- <h1>Your signing up has been successfully completed.</h1>
+      <p>If you would like to login, you can <RouterLink to="/Login">click here</RouterLink>.</p> -->
+    </div>
+</template>
+  
+<script setup lang="ts">
+const token = localStorage.getItem("TOKEN")
+
+const emit = defineEmits(['close-Access-modal', 'change-access']);
+const prop = defineProps(['id' , 'access'])
+function closeAccessModal() {
+    emit('close-Access-modal'); // Emit the 'close-modal' event to the parent component
+}
+
+async function changeAccess() {
+    // emit('change-access')
+    if (prop.access == 'User') {
+        const resualt = await fetch('http://localhost:3000/changeEminency/' + prop.id,{
+        method : "PATCH",
+        headers : {"Content-Type": "application/json" , 'Authorization': token! },
+        body : JSON.stringify({accessLevel : "SubAdmin"})
+    })
+    } else {
+        const resualt = await fetch('http://localhost:3000/changeEminency/' + prop.id,{
+        method : "PATCH",
+        headers : {"Content-Type": "application/json" , 'Authorization': token! },
+        body : JSON.stringify({accessLevel : "User"})
+    })
+    }
+
+    closeAccessModal()
+}
+
+
+</script>
+  
+<style scoped>
+/* Styles for the modal */
+.modalContainer {
+    /* position: relative; */
+    /* left: 30%; */
+    /* margin: 10% 35%; */
+    /* width: 25%; */
+    /* min-width: 100px; */
+    border: 2px solid rgb(171, 88, 238);
+    background-color: white;
+    border-radius: 10px;
+    max-width: 600px;
+    text-align: center;
+    z-index: 4;
+    align-self: center;
+    position: fixed;
+    top: 30%;
+    left: 50%;
+    transform: translateX(-50%);
+}
+
+.modal-bg {
+    background-color: rgba(0, 0, 0, 0.4);
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 3;
+}
+
+.close-button svg {
+    color: rgb(171, 88, 238);
+}
+
+.close-button {
+    float: right;
+    margin-top: 5px;
+    margin-right: 7px;
+    cursor: pointer;
+}
+
+button {
+    color: white;
+    border: none;
+    border-radius: 10px;
+    width: 100px;
+    height: 30px;
+    background: linear-gradient(109.6deg, rgb(128, 144, 233) 17.4%, rgb(171, 88, 238) 52.4%, rgb(255, 129, 246) 91%);
+}
+
+button:hover {
+    border-radius: 10px;
+    background: white;
+    border: 1px solid rgb(171, 88, 238);
+    color: rgb(171, 88, 238);
+    height: 30px;
+    cursor: pointer;
+}
+</style>
+  
--- src/views/ChangePromote.vue
+++ src/views/ChangePromote.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
+import ChangeAccessModal from '../components/ChangeAccessModal.vue'
 const token = localStorage.getItem("TOKEN")
 const Users = ref([{username : '' , email : '' , accessLevel : '' , id:''}])
-
+const subAdmin = ref([{username : '' , email : '' , accessLevel : '' , id:''}])
+let showmodal = ref(false)
+let clickPromote = {
+    id : '',
+    access : ''
+}
 onMounted(async ()=>{
     const resualt = await fetch('http://localhost:3000/showUsers',{headers : { 'Authorization': token! }})
     if (resualt.status == 200) {
@@ -10,27 +16,48 @@ onMounted(async ()=>{
     } else {
         alert('Error' + resualt.status)
     }
-})
 
-async function ChangeToSubAdmin(id :string) {
-    const resualt = await fetch('http://localhost:3000/changeEminency/' + id,{
-        method : "PATCH",
-        headers : {"Content-Type": "application/json" , 'Authorization': token! },
-        body : JSON.stringify({accessLevel : "SubAdmin"})
+    const SubAdminresualt = await fetch('http://localhost:3000//showSubAdmin' , {
+        headers : {'Authorization': token! },
     })
 
-    // alert(resualt.status)
+    if (SubAdminresualt.status == 200) {
+        subAdmin.value = await SubAdminresualt.json()
+    } else {
+        alert('Error ' + SubAdminresualt.status)
+    }
+
     
-    Refetch()
-}
+})
+
+
 
 async function Refetch() {
+    showmodal.value = false
     const resualt = await fetch('http://localhost:3000/showUsers',{headers : { "Authorization": token! }})
     if (resualt.status == 200) {
         Users.value =await resualt.json()
     } else {
         alert('Error' + resualt.status)
     }
+
+
+    const SubAdminresualt = await fetch('http://localhost:3000//showSubAdmin' , {
+        headers : {'Authorization': token! },
+    })
+
+    if (SubAdminresualt.status == 200) {
+        subAdmin.value = await SubAdminresualt.json()
+    } else {
+        alert('Error ' + SubAdminresualt.status)
+    }
+}
+
+
+function Showpanel(userId : string , access : string) {
+    showmodal.value = !showmodal.value
+    clickPromote.id = userId,
+    clickPromote.access = access
 }
 </script>
 
@@ -60,8 +87,35 @@ async function Refetch() {
 
             <h2>{{ user.accessLevel }}</h2>
 
-            <button @click="ChangeToSubAdmin(user.id)">change to subAdmin</button>
+            <button @click="Showpanel(user.id , user.accessLevel)">change to subAdmin</button>
         </div>
+
+        <div class="panelUser" v-for="subadmin in subAdmin" :key="subadmin.email">
+            <div class="username">
+                <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" fill="currentColor" class="bi bi-person-fill"
+                    viewBox="0 0 16 16">
+                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+                </svg>
+
+            <h1 >{{ subadmin.username }}</h1>
+            </div>
+
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-envelope-open-fill" viewBox="0 0 16 16">
+            <path d="M8.941.435a2 2 0 0 0-1.882 0l-6 3.2A2 2 0 0 0 0 5.4v.314l6.709 3.932L8 8.928l1.291.718L16 5.714V5.4a2 2 0 0 0-1.059-1.765l-6-3.2ZM16 6.873l-5.693 3.337L16 13.372v-6.5Zm-.059 7.611L8 10.072.059 14.484A2 2 0 0 0 2 16h12a2 2 0 0 0 1.941-1.516ZM0 13.373l5.693-3.163L0 6.873v6.5Z"/>
+            </svg>
+
+            <h2>{{ subadmin.email }}</h2>
+
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-universal-access-circle" viewBox="0 0 16 16">
+            <path d="M8 4.143A1.071 1.071 0 1 0 8 2a1.071 1.071 0 0 0 0 2.143Zm-4.668 1.47 3.24.316v2.5l-.323 4.585A.383.383 0 0 0 7 13.14l.826-4.017c.045-.18.301-.18.346 0L9 13.139a.383.383 0 0 0 .752-.125L9.43 8.43v-2.5l3.239-.316a.38.38 0 0 0-.047-.756H3.379a.38.38 0 0 0-.047.756Z"/>
+            <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0ZM1 8a7 7 0 1 1 14 0A7 7 0 0 1 1 8Z"/>
+            </svg>
+
+            <h2>{{ subadmin.accessLevel }}</h2>
+
+            <button @click="Showpanel(subadmin.id , subadmin.accessLevel)">change to User</button>
+        </div>
+        <ChangeAccessModal v-if="showmodal" :id="clickPromote.id"  :access="clickPromote.access"  @close-Access-modal="Refetch()"/>
     </div>
 </template>
 
